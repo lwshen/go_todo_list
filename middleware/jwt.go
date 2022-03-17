82c59381ecbabac6af232620a1529e46e1d444ba
Ryo Shen <shenlw66@gmail.com>
2022-03-17 20:48:29 +0800

    middleware check jwt token

---
+++ middleware/jwt.go
@@ -0,0 +1,34 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"time"
+	"todo_list/pkg/utils"
+)
+
+func JWT() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var code int
+		//var data interface{}
+		token := c.GetHeader("Authorization")
+		if token == "" {
+			code = 404
+		} else {
+			claims, err := utils.ParseToken(token)
+			if err != nil {
+				code = 403 //无权限
+			} else if time.Now().Unix() > claims.ExpiresAt {
+				code = 401 //token无效
+			}
+		}
+		if code != 200 {
+			c.JSON(200, gin.H{
+				"status": code,
+				"msg":    "Token解析错误",
+			})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
